cf739d0b1f54c6f46f5b1bffe344114a27665653
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-12-07 15:13:24 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,9 @@ import { concatenation } from "./concatenation.js";
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 if (button && input) {
-    button.addEventListener("click", () => {
-        concatenation(input.value, "hello!");
-    });
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello!");
+  });
 }
-function car(arg) {
-    return console.log(arg);
-}
-car(5);
+
 //# sourceMappingURL=index.js.map
